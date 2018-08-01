f491a81dc93c52996fb8e394a33d4fa59c3f11f0
Donald Sarratt <dsarratt@gmail.com>
2018-08-01 23:41:50 +1200

    Add another challenging puzzle

--- test_sudoku.py
+++ test_sudoku.py
@@ -200,6 +200,33 @@ class SolverTest(unittest.TestCase):
             [7,6,5,2,9,8,1,3,4],
             ])
     
+    def test_escargot(self):
+        """https://www.kristanix.com/sudokuepic/worlds-hardest-sudoku.php"""
+        testgrid = '\n'.join((
+            "1    7 9 ",
+            " 3  2   8",
+            "  96  5  ",
+            "  53  9  ",
+            " 1  8   2",
+            "6    4   ",
+            "3      1 ",
+            " 4      7",
+            "  7   3  ",
+            ))
+        grid = sudoku.initialise(testgrid)
+        self.assertTrue(sudoku.solve(grid))
+        self.assertEqual(grid, [
+            [1,6,2,8,5,7,4,9,3],
+            [5,3,4,1,2,9,6,7,8],
+            [7,8,9,6,4,3,5,2,1],
+            [4,7,5,3,1,2,9,8,6],
+            [9,1,3,5,8,6,7,4,2],
+            [6,2,8,7,9,4,1,3,5],
+            [3,5,6,4,7,8,2,1,9],
+            [2,4,1,9,3,5,8,6,7],
+            [8,9,7,2,6,1,3,5,4],
+            ])
+    
     def test_inkala(self):
         """https://www.telegraph.co.uk/news/science/science-news/9359579/Worlds-hardest-sudoku-can-you-crack-it.html"""
         testgrid = '\n'.join((
